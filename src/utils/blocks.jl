41887bb491e14ff2cb2e414be7c5ce871529276d
Gabriel Previato <gabriel.previato@gmail.com>
2022-09-30 00:06:07 -0300

    .

--- src/utils/blocks.jl
+++ src/utils/blocks.jl
@@ -1,4 +1,34 @@
+using Flux
 
-struct YOLOChain{T<:Union{Tuple, NamedTuple, AbstractVector}}
-    layers::T
+# custom split layer
+struct Split{T}
+  paths::T
 end
+
+Split(paths...) = Split(paths)
+
+Flux.@functor Split
+
+(m::Split)(x::AbstractArray) = map(f -> f(x), m.paths)
+
+struct YOLOChain{T<:AbstractVector}
+    channels::T
+    m::Vector{Any}
+end
+
+function YOLOChain(channels::T) where T<:Union{Tuple, NamedTuple, AbstractVector}
+    return YOLOChain(channels, [])
+end
+
+function Base.getindex(chain::YOLOChain, index::Int)
+    return Base.getindex(chain.channels, index)
+end
+
+function Base.getindex(chain::YOLOChain, index::Vector{Int})
+    return Base.getindex(chain.channels, index)
+end
+
+function Base.length(chain::YOLOChain)
+    return Base.length(chain.channels)
+end
+
--- src/utils/parser.jl
+++ src/utils/parser.jl
@@ -3,38 +3,73 @@ using Flux
 
 using YOLOv7: YOLOChain
 
-struct YOLOBlock{Block}
-    b::Block
-end
+struct YOLOBlock{Block} end
 
 function parse_yolo(file)
     yolo_yaml = YAML.load_file(file; dicttype=Dict{Symbol,Any})
 
-    chain = YOLOChain([])
-    for node in [yolo_yaml[:backbone] ; yolo_yaml[:head]]
-        m = load_node(chain, YOLOBlock(Symbol(node[3])), node[1], node[2], node[4])
-        push!(chain.layers, m)
+    chain = YOLOChain([3], Vector{Any}([[]]))
+    for node in yolo_yaml[:backbone]
+        from = node[1]
+        if isa(from, Vector)
+            from[from .< 0] .= length(chain) .+ from .+ 1
+        else
+            if from < 0
+                from = length(chain) + from + 1
+            end
+        end
+        m = load_node!(chain, YOLOBlock{Symbol(node[3])}(), from, node[2], node[4])
+        println(m)
+        # push!(chain.layers, m)
     end
 
     print(chain)
 end
 
-function load_node(chain::YOLOChain, ::YOLOBlock{:Conv}, from::Int, number::Int, args::Vector{Int})
+function load_node!(chain::YOLOChain, ::YOLOBlock{:Conv}, from::Int, number::Int, args::Vector{Int})
     @assert length(args) == 3
     
+    # Julia does not allow negative index
     ch_in = chain[from]
 
     ch_out = args[1]
     filter = args[2]
     stride = args[3]
-    
-    return Flux.Conv(filter, ch_in => ch_out; stride=stride) 
+
+    if length(filter) == 1
+        filter = (filter, filter)
+    end
+
+    push!(chain.channels, ch_out)
+
+    m = Flux.Conv(filter, ch_in => ch_out; stride=stride) 
+
+    push!(chain.m, m)
+
+    return m
 end
 
-function load_node(chain::YOLOChain, ::YOLOBlock{:Concat}, from::Vector{Int}, number::Int, args::Vector{Int})
+function load_node!(chain::YOLOChain, ::YOLOBlock{:Concat}, from::Vector{Int}, number::Int, args::Vector{Int})
     @assert length(args) == 1
     @assert number == 1
-    
+
     dim = 4 - args[1]
-    return Flux.Join(x -> cat(x; dims=dim), chain[from]...)
+
+    return Flux.Parallel(x -> cat(x; dims=dim), chain.m[from]...)
+end
+
+function load_node!(chain::YOLOChain, ::YOLOBlock{:MP}, from::Int, number::Int, args::Vector{Any})
+    if length(args) == 0
+        window = (2, 2)
+    end
+
+    ch_in = chain[from]
+    ch_out = ch_in
+    
+    push!(chain.channels, ch_out)
+
+    m = Flux.MaxPool(window)
+    push!(chain.m, m)
+
+    return m
 end
