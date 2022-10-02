45a17b449f5d03492df23434ca62bde8e42baaf2
Gabriel Previato <gabriel.previato@gmail.com>
2022-10-02 00:22:24 -0300

    Initial backbone working

--- src/utils/graph.jl
+++ src/utils/graph.jl
@@ -1,21 +1,27 @@
-struct Node
+mutable struct Node
     children::Vector
     parents::Vector
     op::Any
     ch_out::Int
+    node::Any
+    visited::Int
 end
 
-Node() = Node([], [], nothing, 0)
+Node() = Node([], [], nothing, 0, nothing, 0)
 
 function Node(children, parents, op)
     args = op[4]
     if length(args) == 3
         ch_out = args[1]
     else
-        ch_out = 0
+        if length(parents) > 0
+            ch_out = sum(x -> x.ch_out, parents)
+        else
+            ch_out = 0
+        end
     end
 
-    return Node(children, parents, op, ch_out)
+    return Node(children, parents, op, ch_out, nothing, 0)
 end
 
 function Base.show(io::IO, m::Node)
--- src/utils/parser.jl
+++ src/utils/parser.jl
@@ -1,7 +1,7 @@
 using YAML
 using Flux
 
-using YOLOv7: YOLOChain, Node
+using YOLOv7: YOLOChain, Node, Split
 
 struct YOLOBlock{Block} end
 
@@ -9,7 +9,7 @@ function parse_yolo(file)
     yolo_yaml = YAML.load_file(file; dicttype=Dict{Symbol,Any})
 
     # chain = YOLOChain([3], Vector{Any}([[]]))
-    root = Node([], [], (YOLOBlock{:input}(), [0], 1, []), 3)
+    root = Node([], [], (YOLOBlock{:input}(), [0], 1, []), 3, nothing, 0)
     node_list = [root]
     for node in yolo_yaml[:backbone]
         
@@ -41,24 +41,86 @@ function parse_yolo(file)
     return node_list
 end
 
-function parse_graph(graph::Vector{Node})
-    layers = []
-    actual_chain = []
-    for node in graph
-        n = load_node(graph, node.op...)
-        push!(actual_chain, n)
-
-        if length(node.children) == 1
-            
-        else
-            n = Split()
-        end
-        println(n)
+function parse_node(graph, node::Node)
+    node.visited += 1
+    to_visit = nothing
+    node.node = load_node(graph, node)
+    actual_chain::Vector{Any} = []
+    if node.node !== nothing
+        actual_chain = [node.node]
     end
+
+    if length(node.parents) == 1 || node.op[1] == YOLOBlock{:input}()
+        if length(node.children) == 1
+            n, to_visit = parse_node(graph, node.children[1])
+            push!(actual_chain, n...)
+        elseif length(node.children) > 1
+            # n = load_node(graph, c.op...)
+            children_chain = []
+            for c in node.children
+                n_child, to_visit = parse_node(graph, c)
+                push!(children_chain, n_child)
+            end
+            u = Parallel((x...) -> cat(x...; dims=3), children_chain...)
+            push!(actual_chain, u)
+
+            if to_visit !== nothing
+                println("visiring")
+                u = parse_node(graph, to_visit)[1]
+                push!(actual_chain, u)
+            end
+        else
+        end
+    else
+        if node.visited == length(node.parents)
+            to_visit = node.children[1]
+        # u = parse_node(graph, c)
+        #     push!(actual_chain, u)
+        #     # println(u)
+        end
+    end
+
+    return Flux.Chain(actual_chain), to_visit
 end
 
+function parse_graph(graph::Vector{Node})
+    layers = []  
+    
+    s::Node = graph[1]
+
+    parse_node(graph, s)
+
+    # n = load_node(graph, s.op...)
+    # explore = [n]
+    # actual_chain = []
+    # while !isempty(explore)
+    #     u = pop!(explore)
+    #     println(u)
+    #     push!(actual_chain, u)
+
+    #     if length(u.children) > 1
+    #         children_chains, next = parse_children(graph, u)
+    #         u = Split(children_chains)
+    #         push!(actual_chain, u)
+            
+    #         push!(layers, actual_chain)
+    #         actual_chain = []
+    #     else
+    #         next = u.children[1]
+    #     end
+
+    #     push!(explore, next)
+    # end
+
+    # push!(layers, actual_chain)
+    # println(layers)
+end
+
+load_node(chain, node::Node) = load_node(chain, node.op...)
+
 function load_node(chain, ::YOLOBlock{:input}, from::Vector{Int}, number::Int, args::Vector)
-    return ""
+    # return chain[1]
+    return nothing
 end
 
 function load_node(chain, ::YOLOBlock{:Conv}, from::Vector{Int}, number::Int, args::Vector{Int})
@@ -77,7 +139,7 @@ function load_node(chain, ::YOLOBlock{:Conv}, from::Vector{Int}, number::Int, ar
 
     # push!(chain.channels, ch_out)
 
-    m = Flux.Conv(filter, ch_in => ch_out; stride=stride) 
+    m = Flux.Conv(filter, ch_in => ch_out; stride=stride, pad=Flux.SamePad()) 
 
     # push!(chain.m, m)
 
@@ -90,7 +152,10 @@ function load_node(chain, ::YOLOBlock{:Concat}, from::Vector{Int}, number::Int, 
 
     dim = 4 - args[1]
 
-    return Flux.Parallel(x -> cat(x; dims=dim), map(x -> x.ch_out, chain[from])...)
+    m = Flux.Parallel(x -> cat(x; dims=dim), map(x -> x.node, chain[from])...)
+    # println(m)
+
+    return nothing
 end
 
 function load_node(chain, ::YOLOBlock{:MP}, from::Vector{Int}, number::Int, args::Vector{Any})
@@ -108,3 +173,7 @@ function load_node(chain, ::YOLOBlock{:MP}, from::Vector{Int}, number::Int, args
 
     return m
 end
+
+data = parse_graph(parse_yolo(raw"C:\Users\Previato\YOLOv7.jl\src\conf\yolov7.yaml"))
+m = Flux.Chain(data[2:end])
+println(m)
