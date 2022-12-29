2c37b7e399d17595b134be96265b187d55ffaccf
Gabriel Previato <gabriel.previato@gmail.com>
2022-12-29 15:19:00 -0300

    ..

--- src/YOLOv7.jl
+++ src/YOLOv7.jl
@@ -4,7 +4,8 @@ include("utils/activations.jl")
 include("utils/graph.jl")
 include("utils/blocks.jl")
 include("utils/parser.jl")
-# export YOLOChain
+
+export load_model
 
 println("Imported YOLOv7")
 end # module
